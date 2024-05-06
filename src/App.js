53dcb03f79cbaa45e85d1415c2fa979d1632cab8
Alexander_Ovchar <azalexxx02@gmail.com>
2024-05-06 23:49:36 +0300

    complete table project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Canvas } from '@react-three/fiber';
 import { Experience } from 'components/Experience';
+import { Interface } from 'components/Interface';
 
 function App() {
   return (
     <div className='h-screen w-screen'>
-      <Canvas shadows camera={{ position: [4, 0, -12], fov: 35 }}>
+      <Canvas shadows camera={{ position: [4, 4, -12], fov: 35 }}>
         <Experience />
       </Canvas>
+      <Interface />
     </div>
   );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import { OrbitControls, Stage } from '@react-three/drei';
+import { useConfigurator } from 'contexts/Configurator';
+import React from 'react';
+
+import { Table } from './Table';
 
 export const Experience = () => {
+  const { legs } = useConfigurator();
   return (
     <>
-    <mesh>
-      <boxGeometry></boxGeometry>
-      <meshStandardMaterial attach="material" color="hotpink" />
-    </mesh>
+      {/* <mesh
+        rotation-x={-Math.PI * 0.5}
+        castShadow
+        receiveShadow
+        position={[0, -1, 0]}
+      >
+        <planeGeometry args={[30, 30]} />
+      </mesh> */}
+      <Stage
+        intensity={1.5}
+        environment="city"
+        shadows={{
+          type: 'accumulative',
+          color: '#d9afd9',
+          colorBlend: 0,
+          opacity: 1,
+        }}
+        adjustCamera={2}
+      >
+        <Table />
+      </Stage>
+      <OrbitControls
+        makeDefault
+        minPolarAngle={0}
+        maxPolarAngle={Math.PI / 2}
+      />
     </>
-  )
-}
+  );
+};
