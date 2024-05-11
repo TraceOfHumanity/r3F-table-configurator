f7dd8164ff6046db9328357cbe3dd262aace3910
Alexander_Ovchar <azalexxx02@gmail.com>
2024-05-11 22:39:30 +0300

    fix h-screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { Canvas } from '@react-three/fiber';
+
 import { Experience } from 'components/Experience';
 import { Interface } from 'components/Interface';
 
 function App() {
   return (
-    <div className='h-screen w-screen'>
-      <Canvas shadows camera={{ position: [4, 4, -12], fov: 35 }}>
+    <>
+      <Canvas className="" shadows camera={{ position: [4, 4, -12], fov: 35 }}>
         <Experience />
       </Canvas>
       <Interface />
-    </div>
+    </>
   );
 }
 
